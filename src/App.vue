c0e973388894ba6f437b85a62a0f273c56a98ebd
CLorant <113087475+CLorant@users.noreply.github.com>
2023-03-23 21:42:28 +0100

    felhasználó bejelentkeztetés elkezdve

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <script>
-  import Nav from './components/Nav.vue'
-  import Footer from './components/Footer.vue'
+  import Nav from './components/Nav.vue';
+  import Footer from './components/Footer.vue';
+  import { mapWritableState } from 'pinia';
+  import { useFelhasznaloStore } from './stores/felhasznalo';
+  import felhasznaloJSON from './felhasznalo.json'; // átmeneti
+  import axios from 'axios';
 
   export default {
     components: {
       Nav,
       Footer
+    },
+
+    computed: {
+      ...mapWritableState(useFelhasznaloStore, ['felhasznalo'])
+    },
+
+    beforeMount() {
+      // automatikus bejelentkezést ide majd valahogy
+      /*
+      try {
+        const getRes = axios.get('/api/getUserByName', {
+          params: {
+            felhasznalo: "asd" 
+          }
+        });
+        for (const prop in this.felhasznalo) { 
+          if (getRes.hasOwnProperty(prop)) {
+            this.felhasznalo[prop] = getRes[prop];
+          }
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      */
+
+      const res = felhasznaloJSON // átmeneti
+      for (const prop in this.felhasznalo) { 
+        if (res.hasOwnProperty(prop)) {
+          this.felhasznalo[prop] = res[prop];
+        }
+      }
+      this.felhasznalo.bejelentkezett = true // átmeneti
+      this.felhasznalo.jogosultsag = "admin" // átmeneti
     }
   }
 </script>
