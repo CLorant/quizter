acc60994f0410abba68c0f59c4b7cea4707d5842
KatamoriVagyok <116543333+KatamoriVagyok@users.noreply.github.com>
2023-04-29 18:53:47 +0200

    Elfelejtett jelszo gaming

--- backend/backend/routes/auth/validation.js
+++ backend/backend/routes/auth/validation.js
@@ -4,6 +4,10 @@ exports.RegisterValidation = (
 [
     check('username').isLength({ min: 3, max:12 }).withMessage("Név legalább 3 karakterből és maximum 12 karakterből áljon!").matches(/^(?=.{3,12}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/).trim(),
     check('email').isEmail().withMessage("Nem email").normalizeEmail(),
-    check('password').isLength({min: 8}).withMessage("Nem megfelelő a jelszó, 8 karakter min").matches(/\d/).withMessage("Jelszóba legyen 1 szám"),
+    check('password').isLength({min: 8}).withMessage("Fos a jelszó, 8 karaktert").matches(/\d/).withMessage("Jelszóba legyen 1 szám"),
     check('password2').custom((value, { req, res }) => {if(value !== req.body.password){return false;} return true;}).withMessage("Nem azonos jelszó")
 ]);
+exports.ChangePWValidation = ([
+    check('password').isLength({min: 8}).withMessage("Fos a jelszó, 8 karaktert").matches(/\d/).withMessage("Jelszóba legyen 1 szám"),
+    check('password2').custom((value, { req, res }) => {if(value !== req.body.password){return false;} return true;}).withMessage("Nem azonos jelszó")
+])
